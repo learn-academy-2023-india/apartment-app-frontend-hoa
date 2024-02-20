97e0dae1adf768083210c7598750668172eeb75c
xdfripp@gmail.com <xdfripp@gmail.com>
2024-02-20 14:39:07 -0800

    corrections made

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React, { useState } from "react"
-import mockUsers from "./mockUsers";
-import mockApartments from "./mockApartments";
-import logo from './logo.svg';
-import './App.css';
 import { Routes, Route } from "react-router-dom"
+import mockUsers from "./mockUsers.js"
+import mockApartments from "./mockApartments.js"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import ApartmentIndex from "./pages/ApartmentIndex"
+import ApartmentShow from "./pages/ApartmentShow"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import NotFound from "./pages/NotFound"
+import ApartmentProtectedIndex from "./pages/ApartmentProtectedIndex"
+import ApartmentNew from "./pages/ApartmentNew"
 
-function App() {
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(mockUsers[0])
+  const [apartments, setApartments] = useState(mockApartments)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apartmentindex" element={<ApartmentIndex />} />
+          <Route path="/catshow" element={<ApartmentShow />} />
+          <Route path="/catnew" element={<ApartmentNew />} />
+          <Route path="/catedit" element={<ApartmentProtectedIndex />} />
+          <Route path="/catedit" element={<SignIn />} />
+          <Route path="/catedit" element={<SignUp />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      <Footer />
+    </>
+  )
 }
 
 export default App;
