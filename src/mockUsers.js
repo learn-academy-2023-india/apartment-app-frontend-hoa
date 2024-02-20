99c8b77ba80313c81c3b3a79020d637227f22e45
xdfripp@gmail.com <xdfripp@gmail.com>
2024-02-20 14:16:47 -0800

    completed react components

---
+++ src/mockUsers.js
@@ -0,0 +1,16 @@
+let mockUsers = [
+    {
+        id: 1,
+        email: "jimmydontjam@gmail.com"
+    },
+    {
+        id: 2,
+        email: "irvinyomomma@gmail.com"
+    },
+    {
+        id: 2,
+        email: "frippdiesel@gmail.com"
+    }
+]
+
+export default mockUsers
---
+++ src/pages/apartmentNew.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const ApartmentNew = () => {
+  return (
+    <>
+
+    </>
+  )
+} 
+
+export default ApartmentNew
