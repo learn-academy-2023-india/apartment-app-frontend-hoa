f09ad861c0ac3a3bc21b7c7fa0e986b8aadb728e
xdfripp@gmail.com <xdfripp@gmail.com>
2024-02-20 16:34:24 -0800

    finished react header & footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React from 'react'
+import { Nav } from 'reactstrap'
+import { NavLink, useNavigate } from "react-router-dom";
 
-const Header = () => {
+const Header = (currentUser) => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate("/")
+  }
+
   return (
     <>
-
+        <h1>Apartments Homepage</h1>
     </>
   )
-} 
+}
 
 export default Header
