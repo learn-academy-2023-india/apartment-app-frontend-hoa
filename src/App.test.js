5b2a4918c24aca18bca9f364c236d4a0ab024dae
jayfr1122 <jayfranks21@gmail.com>
2024-02-21 13:11:01 -0800

    home and not found styling and testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, image } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe("<App />", () => {
+  it("renders an image", () => {
+    render (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
+    })
+  it("renders an nav list", () => {
+    render (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
+    const list = screen.getByRole('link', {
+      name: /contact/i})
+    expect(list).toBeInTheDocument()
+  })
+  })
