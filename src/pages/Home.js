7e1274be8e99fb3587e5b06007638187336ba4ff
jayfr1122 <jayfranks21@gmail.com>
2024-02-21 16:27:33 -0800

    deleted unneeded code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,21 +11,3 @@ const Home = () => {
 } 
 
 export default Home
-
-// import React from 'react'
-// import Video from '../assets/video.mp4'
-
-// const Home = () => {
-//   return (
-//     <div className='video'>
-//       <video autoPlay loop muted playsInline id="video">
-//         <source role="video" src={Video} type="video/mp4" />
-//       </video>
-//         <div className="overlay">
-//           <h1 className="title">Welcome to The Bricks </h1>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Home
